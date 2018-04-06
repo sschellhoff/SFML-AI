275e2f575450b314a25392225e42a3daaef10166
sschellhoff <soeren.schellhoff@gmail.com>
2018-04-06 15:22:21 +0200

    added some Vector utilities

---
+++ src/VectorUtils.cpp
@@ -0,0 +1,45 @@
+/*
+MIT License
+
+Copyright (c) 2018 Sören Schellhoff
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#pragma once
+
+#include "mercer/Misc/VectorUtils.hpp"
+
+namespace mercer {
+
+float vector_length_squared(const sf::Vector2f &vector) {
+    return vector.x * vector.x + vector.y * vector.y;
+}
+
+float vector_length(const sf::Vector2f &vector) {
+    auto length_squared = vector_length_squared(vector);
+    return length_squared * length_squared;
+}
+
+sf::Vector2f process_deadzone(sf::Vector2f vector, float deadzone) {
+    auto length_squared = vector_length_squared(vector);
+    return (length_squared >= deadzone) ? vector : ((length_squared <= -deadzone) ? vector : sf::Vector2f{});
+}
+
+}
