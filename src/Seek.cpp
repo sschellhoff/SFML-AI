f61659a03fb8a6bdb43a79460a1aa4750b53f935
sschellhoff <soeren.schellhoff@gmail.com>
2018-04-06 15:34:45 +0200

    added seek behavior

---
+++ src/Seek.cpp
@@ -0,0 +1,38 @@
+/*
+MIT License
+
+Copyright (c) 2018 Sören Schellhoff
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#pragma once
+
+#include "mercer/AI/Steering/Seek.hpp"
+#include "mercer/Misc/VectorUtils.hpp"
+
+namespace mercer {
+
+sf::Vector2f seek(const sf::Vector2f &target_position, const sf::Vector2f &own_position, const sf::Vector2f &own_velocity, float max_speed) {
+    sf::Vector2f desired_velocity = get_normalized(target_position - own_velocity) * max_speed;
+
+    return desired_velocity - own_velocity;
+}
+
+}
